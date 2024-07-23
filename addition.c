6a760fbf75577ad187f420320d4d1c4bceedc9d7
Liya V S <liyavsebastian2003@gmail.com>
2024-07-23 21:33:40 +0530

    Create addition.c
    program to add two numbers

---
+++ addition.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int number1,number2;
+    printf("Enter two integers = ");
+    scanf("%d%d",&number1,&number2);
+    printf("Sum = %d",number1+number2);
+ return 0;
+}
